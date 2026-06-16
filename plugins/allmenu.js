f2948737c57f1c6be087e2c9a4ba297594231527
𝟵ᴍᴀɴ-𝗫-𝗬ᴀᴍ𝗗ʜᴜᴅ <kingkhannoman2@gmail.com>
2026-06-16 16:09:23 +0530

    Change module import from arslan to noman

--- plugins/allmenu.js
+++ plugins/allmenu.js
@@ -1,4 +1,4 @@
-const { cmd, commands } = require("../arslan");
+const { cmd, commands } = require("../noman");
 const moment = require("moment-timezone");
 const { fakevCard } = require('../lib/fakevCard');
 
